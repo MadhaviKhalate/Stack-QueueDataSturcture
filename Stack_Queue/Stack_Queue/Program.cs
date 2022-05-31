9cc2a6f8d732347e50eef15f12deaf24d5a9a0c5
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-31 18:04:00 +0530

    [Madhavi] Peak and pop elements from stack

--- Stack_Queue/Stack_Queue/Program.cs
+++ Stack_Queue/Stack_Queue/Program.cs
@@ -6,21 +6,28 @@ namespace Stack_Queue
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("1.Creating Stack");
+            Console.WriteLine("1.Creating Stack \n2.Peak and Pop Element");
             Console.WriteLine("Enter your option:");
             int option = Convert.ToInt32(Console.ReadLine());
             StackAndQueue stack = new StackAndQueue();
             switch (option)
             {
                 case 1:
-                        stack.PushElements(70);
-                        stack.PushElements(30);
-                        stack.PushElements(56);
-                        stack.DisplayElements();
-                        break;
+                    stack.PushElements(70);
+                    stack.PushElements(30);
+                    stack.PushElements(56);
+                    stack.DisplayElements();
+                    break;
 
                 case 2:
-
+                    stack.PushElements(70);
+                    stack.PushElements(30);
+                    stack.PushElements(56);
+                    stack.DisplayElements();
+                    stack.PeakElement();
+                    stack.PopElement();
+                    stack.IsEmpty();
+                    stack.DisplayElements();
                     break;
 
                 default:
--- Stack_Queue/Stack_Queue/StackAndQueue.cs
+++ Stack_Queue/Stack_Queue/StackAndQueue.cs
@@ -41,5 +41,34 @@ namespace Stack_Queue
                 temp = temp.next;
             }
         }
+
+        public void PeakElement()
+        {
+            Console.WriteLine();
+            if (this.top == null)
+            {
+                Console.WriteLine("The Stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is top of the stack", this.top.data);
+        }
+        public void PopElement()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("The Stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is popped element from stack", this.top.data);
+            this.top = this.top.next;
+        }
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                PeakElement();
+                PopElement();
+            }
+        }
     }
 }
