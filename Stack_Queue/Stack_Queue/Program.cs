ee572a1ed9d5847cb9590d7895c4f9a57990db5b
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-31 17:53:41 +0530

    [Madhavi] create stack

---
+++ Stack_Queue/Stack_Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stack_Queue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1.Creating Stack");
+            Console.WriteLine("Enter your option:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            StackAndQueue stack = new StackAndQueue();
+            switch (option)
+            {
+                case 1:
+                        stack.PushElements(70);
+                        stack.PushElements(30);
+                        stack.PushElements(56);
+                        stack.DisplayElements();
+                        break;
+
+                case 2:
+
+                    break;
+
+                default:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Stack_Queue/Stack_Queue/StackAndQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    internal class StackAndQueue
+    {
+        private Node top;
+        private Node head = null;
+        public StackAndQueue()
+        {
+            this.top = null;
+        }
+        public void PushElements(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed into Stack", value);
+        }
+        public void DisplayElements()
+        {
+            Node temp = this.top;
+            if (this.top == null)
+            {
+                Console.WriteLine("Stacks is Empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
